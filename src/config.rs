410b0a4b5e80127a03f9af8a41168f2217d4a8cb
darrylpargeter <darrylpargeter@gmail.com>
2019-09-26 19:19:37 +0100

    added update host for existing hosts files

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,15 @@ use toml;
 use std::fs;
 use toml::Value;
 
+// for file reading
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use regex::{Regex, NoExpand};
+
+const DELIMT_START: &str = "### quacken shortcuts :start ###\n";
+const DELIMT_END: &str = "### quacken shortcuts :end ###\n";
+
 pub fn get_config() -> toml::Value {
     let file_name: &str = "./config.toml";
 
@@ -13,4 +22,43 @@ pub fn get_config() -> toml::Value {
     let config = file_contents.parse::<Value>().unwrap();
 
     config
+}
+
+pub fn update_config() {
+    let path = "/etc/hosts";
+    let config = get_config();
+    let table = config.as_table().unwrap();
+    let mut replacemnt = String::new();
+
+    // generate payload
+    replacemnt.push_str(DELIMT_START);
+    for key in table.keys() {
+        replacemnt.push_str(format!("127.0.0.1 {}\n", key).as_str());
+    }
+    replacemnt.push_str(DELIMT_END);
+
+    // TODO get the config file and update its contents
+    let mut buffer = String::new();
+    //let mut hosts = File::open("/etc/hosts")
+    let mut hosts = File::open(path)
+        .expect("File not found or cannot be opened");
+
+    hosts.read_to_string(&mut buffer)
+        .unwrap();
+
+    let re = Regex::new(DELIMT_START).unwrap();
+    let mut updated_content: String = String::new();
+
+    if !re.is_match(buffer.as_str()) {
+        let mut data = buffer.to_owned();
+        let updatedHost = replacemnt.to_owned();
+        data.push_str(&updatedHost);
+        updated_content = data;
+    } else {
+        let re = Regex::new(r"(###(.*)##)\n(.|\n)*(###(.*)##\n)").unwrap();
+        updated_content = re.replace_all(buffer.as_str(), NoExpand(replacemnt.as_str())).into_owned();
+    }
+
+    // write the content to the file
+    fs::write(path, updated_content).expect("Unable to write file");
 }
--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,8 @@ use quacken::{ config };
 use quacken::token::Token;
 use std::path::PathBuf;
 use rocket::response::Redirect;
-use regex::Regex;
-
-// for file reading
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
-use std::fs;
 
 use hotwatch::{Hotwatch, Event};
-
-const DELIMT_START: &str = "### quacken shortcuts :start ###\n";
-const DELIMT_END: &str = "### quacken shortcuts :end ###\n";
 
 // TODO move this into its own file 
 // if you can 
@@ -49,45 +39,7 @@ fn main() {
     //  TODO unhard code the config.toml path
     hotwatch.watch("./config.toml", |event: Event| {
         if let Event::Write(path) = event {
-            let path = "/etc/hosts";
-            let config = config::get_config();
-            let table = config.as_table().unwrap();
-            let mut replacemnt = String::new();
-
-            // generate payload
-            replacemnt.push_str(DELIMT_START);
-            for key in table.keys() {
-                replacemnt.push_str(format!("127.0.0.1 {}\n", key).as_str());
-            }
-            replacemnt.push_str(DELIMT_END);
-
-            // TODO get the config file and update its contents
-            let mut buffer = String::new();
-            //let mut hosts = File::open("/etc/hosts")
-            let mut hosts = File::open(path)
-                .expect("File not found or cannot be opened");
-
-            hosts.read_to_string(&mut buffer)
-                .unwrap();
-
-            let re = Regex::new(DELIMT_START).unwrap();
-            if !re.is_match(buffer.as_str()) {
-                // TODO write to file
-                println!("{:#?}", buffer);
-                let mut data = buffer.to_owned();
-                let updatedHost = replacemnt.to_owned();
-                data.push_str(&updatedHost);
-                // fs::write("/etc/hosts", format!("{}{}", buffer, replacemnt));
-                println!("data: {}", data);
-                fs::write(path, data).expect("Unable to write file");
-            } else {
-                // TODO update file
-                println!("quacken exists");
-            }
-            /*
-            for line in buf_read.lines() {
-            }
-            */
+            config::update_config();
         }
     }).expect("failed to watch file!");
     // start the web server with the bash path
